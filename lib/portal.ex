dbf4e95a90557205844ea65163db2976c207fc60
clay richardson <clayrichardson@users.noreply.github.com>
2016-04-29 22:43:08 -0700

    finished

--- lib/portal.ex
+++ lib/portal.ex
@@ -8,12 +8,12 @@ defmodule Portal do
 
     children = [
       # Define workers and child supervisors to be supervised
-      # worker(Portal.Worker, [arg1, arg2, arg3]),
+      worker(Portal.Door, [])
     ]
 
     # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
     # for other strategies and supported options
-    opts = [strategy: :one_for_one, name: Portal.Supervisor]
+    opts = [strategy: :simple_one_for_one, name: Portal.Supervisor]
     Supervisor.start_link(children, opts)
   end
   
@@ -45,6 +45,13 @@ defmodule Portal do
 
     # Let's return the portal itself.
     portal
+  end
+  
+  @doc """
+  Shoots a new door with the given `color`.
+  """
+  def shoot(color) do
+    Supervisor.start_child(Portal.Supervisor, [color])
   end
 end
 
